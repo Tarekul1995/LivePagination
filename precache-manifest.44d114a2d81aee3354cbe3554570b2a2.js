d354abf2bbade120f33ab578324994e946a3384d
Tarekul1995 <islamtarek4@gmail.com>
2019-08-19 12:57:28 +0600

    Updates

---
+++ precache-manifest.44d114a2d81aee3354cbe3554570b2a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5422579fdb694cf059e772d2e5c324",
+    "url": "/liveurlgithub/index.html"
+  },
+  {
+    "revision": "754ff8c70ff67e884a7e",
+    "url": "/liveurlgithub/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "aa724896dfb39d87ab27",
+    "url": "/liveurlgithub/static/js/2.0b91b939.chunk.js"
+  },
+  {
+    "revision": "754ff8c70ff67e884a7e",
+    "url": "/liveurlgithub/static/js/main.872dae57.chunk.js"
+  },
+  {
+    "revision": "baa367ebdf9eac258a0d",
+    "url": "/liveurlgithub/static/js/runtime~main.67322020.js"
+  }
+]);
